cb96916ceb34835f95caecbcaf1a360f8b9210a4
gogl13 <01064767065jh@gmail.com>
2021-05-08 21:51:34 +0900

    add.1338.js

---
+++ javascript/1330.js
@@ -0,0 +1,13 @@
+//let input = require("fs").readFileSync('/dev/stdin').toString().split(' ');
+let input = fs.readFileSync("./1300.js").toString().split("\n");
+
+const a = parseInt(input[0]);
+const b = parseInt(input[1]);
+
+if (a < b) {
+  console.log("<");
+} else if (a > b) {
+  console.log(">");
+} else {
+  console.log("==");
+}
