9af04b767c67ace9a3dbcb23a3dc4230dd276e39
gogl13 <01064767065jh@gmail.com>
2021-05-10 23:19:32 +0900

    add.9498.js

--- javascript/9498.js
+++ javascript/9498.js
@@ -1,4 +1,4 @@
-let input = require("fs").readFileSync("/dev/stdin").toString().split(" ");
+1let input = require("fs").readFileSync("/dev/stdin").toString().split(" ");
 //let input = fs.readFileSync("./1300.js").toString().split("\n");
 const a = parseInt(input[0]);
 
