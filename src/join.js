b66818371e1d86606c8bada9dfab006749c701e5
gogl13 <01064767065jh@gmail.com>
2021-05-14 08:14:13 +0900

    add.join.js

--- src/join.js
+++ src/join.js
@@ -3,6 +3,7 @@ const closeiconElement = document.querySelector(".closeicon");
 const closedmodalElement = document.querySelector(".closedmodal");
 const peoplejoinElement = document.querySelector(".peoplejoin");
 const no2Element = document.querySelector(".no2");
+const modalcontentsElement = document.querySelector(".modalcontents");
 
 peoplejoinElement.addEventListener("click", function (event) {
   modalfullElement.classList.remove("closedmodal");
@@ -14,4 +15,8 @@ closeiconElement.addEventListener("click", function (event) {
 
 modalfullElement.addEventListener("click", function (event) {
   closedmodalElement.classList.add("closedmodal");
+});
+
+modalcontentsElement.addEventListener("click", (event) => {
+  event.stopPropagation();
 });
