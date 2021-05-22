1aa8742e8f7f884b7791c5b3c507b7d538a365c5
gogl13 <01064767065jh@gmail.com>
2021-05-22 21:42:23 +0900

    add.1330.js

--- javascript/1330.js
+++ javascript/1330.js
@@ -6,10 +6,10 @@ let input = require("fs").readFileSync("./input.txt").toString().split(" ");
 const a = Number(input[0]);
 const b = Number(input[1]);
 
-if (a < b) {
-  console.log("<");
-} else if (a < b) {
+if (a > b) {
   console.log(">");
+} else if (a < b) {
+  console.log("<");
 } else {
-  console.log("==");
+  console.log("===");
 }
