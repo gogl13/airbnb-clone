33c4291b329b802bdfa23b368c533833e6d495f2
gogl13 <01064767065jh@gmail.com>
2021-05-25 23:06:35 +0900

    add.2739.js

--- javascript/2739.js
+++ javascript/2739.js
@@ -1,10 +1,18 @@
-const fs = require("fs");
-let stdin =
-  process.platform === "linux" ? fs.readFileSync("/dev/stdin").toString() : `2`;
+// const fs = require("fs");
+// let stdin = process.platform === "linux" ? fs.readFileSync("/dev/stdin").toString() : `2`;
 
-let number2 = parseInt(stdin);
+let input = require("fs").readFileSync("./input.txt").toString().split(" ");
 
-for (let i = 1; i <= 9; i++) {
-  let gogo = number2 * i;
-  console.log(`${number2} * ${i} = ${gogo}`);
+let num = Number(input[0]);
+
+for (a = 1; a <= 9; a++) {
+  console.log(`${num} * ${a} = ${num * a}`);
 }
+
+// let input = require("fs").readFileSync("./input.txt").toString().split(" ");
+
+// let num = Number(input[0]);
+
+// for (let i = 1; i < 10; i++) {
+// console.log(`${num} * ${i} = ${num * i}`);
+// }
