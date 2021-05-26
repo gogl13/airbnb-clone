21fab3e959cefd76a79788e65202a7d386848569
gogl13 <01064767065jh@gmail.com>
2021-05-27 02:02:53 +0900

    add.11021.js

---
+++ javascript/11021.js
@@ -0,0 +1,10 @@
+let input = require("fs").readFileSync("./input.txt").toString().split("\n");
+
+// let input = require("fs").readFileSync("/dev/stdin").toString();.split("\n")
+// let input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
+
+for (let a = 1; a <= input[0]; a++) {
+  let number = input[a].split(" ");
+
+  console.log(`Case #${a}: ${Number(number[0]) + Number(number[1])}`);
+}
