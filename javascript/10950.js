9ca7d91dbd13cef3f6f2491bc168073b38dde48f
gogl13 <01064767065jh@gmail.com>
2021-06-05 19:19:55 +0900

    add.10950.js

--- javascript/10950.js
+++ javascript/10950.js
@@ -1,16 +1,23 @@
-let input = require("fs").readFileSync("./input.txt").toString().split(" ");
+let input = require("fs").readFileSync("./input.txt").toString().split("\n");
 
-// let input = require("fs").readFileSync("/dev/stdin").toString().split(' ');
+//테스트의 갯수는 n이다.
+//a 와 b를 더한다.
 
-// for (let a = 1; a <= input[0]; a++) {
-//   let number = input[a].split(' ');
-//   console.log(Number(number[0]) + Number(number[1]));
-// }
+/*5 , 1 1, 2 2, 3 3, 4 4, 5 5 */
 
-let input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
+// let test = "100 200";
+// let splitinput = test.split(" "); // ['100', '100']
 
-for (let i = 1; i <= input[0]; i++) {
-  let numbers = input[i].split(" ");
+// let a = Number(splitinput[0]);
+// let b = Number(splitinput[1]);
 
-  console.log(Number(numbers[0]) + Number(numbers[1]));
+// console.log(a + b);
+
+let n = Number(input[0]);
+//input: [ '5', '1 1', '2 2', '3 3', '4 4', '5 5' ]
+for (i = 1; i <= n; i++) {
+  let splitinput = input[i].split(" ");
+  let a = Number(splitinput[0]);
+  let b = Number(splitinput[1]);
+  console.log(a + b);
 }
