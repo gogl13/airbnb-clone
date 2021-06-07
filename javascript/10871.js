80fcbdd093a61a406d8192dd9493e1cef8236f6e
gogl13 <01064767065jh@gmail.com>
2021-06-07 23:08:42 +0900

    add.10871.js

--- javascript/10871.js
+++ javascript/10871.js
@@ -1,15 +1,24 @@
 let fs = require("fs");
 // let input = fs.readFileSync('/dev/stdin').toString().split('\n');
-let input = fs.readFileSync("./input.txt").toString().split("\n");
+let input = fs.readFileSync("./input.txt").toString();
 
-let readlineIdx = 0;
-const readInput = () => input[readlineIdx++];
+//10 5
+//1 10 4 9 2 3 8 5 7 6
 
-function main() {
-  let [n, x] = readInput().split(" ");
-  let arr = readInput().split(" ");
-  console.log(n, x);
-  console.log(arr);
+input = input.split("\n");
+// ['10 5', '1 4 9 2 3 8 5 7 1 1 1']
+
+let n = Number(input[0].split(" ")[0]); //'10 5' >>['10', '5']>> '10'
+let x = Number(input[0].split(" ")[1]);
+let list = input[1].split(" "); //'1 10 4 9 2 3 8 5 7 6' >> ['1', '10', '4', '9', '2', '3', '8', '5', '7', '6']
+
+for (let i = 0; i < n; i++) {
+  //  let value = list[0]; // ['1', '4', '9', '2', '3', '8', '5', '7', '1' ] >>
+  let value = Number(list[i]);
+
+  if (value < x) {
+    // console.log(`${value} `);
+    process.stdout.write(`${value} `);
+    // console.log(value + " ")
+  }
 }
-
-main();
