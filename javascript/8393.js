70fe0cb098e041ac872edd006e120caf30ec2e54
gogl13 <01064767065jh@gmail.com>
2021-06-08 23:08:36 +0900

    add.8393.js

--- javascript/8393.js
+++ javascript/8393.js
@@ -2,9 +2,14 @@ let input = require("fs").readFileSync("./input.txt").toString().split(" ");
 
 // let input = require("fs").readFileSync("/dev/stdin").toString().split(' ');
 
-let number = input[0];
-let answer = 0;
-for (let a = 1; a <= number; a++) {
-  answer = answer + a;
+let n = Number(input[0]);
+
+let sum = 0;
+
+for (let i = 1; i <= n; i++) {
+  sum += i;
 }
-console.log(answer);
+console.log(sum);
+
+//
+//
