ad17cf436b1db542d1d8b1e1a9d74a52e304dae1
gogl13 <01064767065jh@gmail.com>
2021-06-11 23:14:56 +0900

    add.test2105.js

--- javascript/test2105.js
+++ javascript/test2105.js
@@ -1,8 +1,8 @@
 let sum = 0;
-let value = 1; //while문에 조건을 맞출 변수 && 하나씩 숫자가 증가하는것을 나타내려함.
+let value = 0; //while문에 조건을 맞출 변수 && 하나씩 숫자가 증가하는것을 나타내려함.
 //sum = 66
 //value = 11
-while (value <= 10) {
+while (value < 10) {
   value += 1;
   sum = value + sum;
 
