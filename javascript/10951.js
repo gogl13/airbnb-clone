63bdc63a2410a2ea9d999e24ca030bfd23f966b9
gogl13 <01064767065jh@gmail.com>
2021-06-12 11:04:36 +0900

    delete

--- javascript/10951.js
+++
@@ -1,8 +0,0 @@
-// let input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
-let input = fs.readFileSync("./input.txt").toString().split("\n");
-
-for (let a = 0; a < input.length - 1; a++) {
-  let number = input[a].split(" ");
-
-  console.log(Number(number[0]) + Number(number[1]));
-}
--- javascript/2438.js
+++
@@ -1,9 +0,0 @@
-let input = require("fs").readFileSync("./input.txt").toString().split("\n");
-// let input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
-
-let star = "";
-
-for (a = 1; a <= input; a++) {
-  star += "*";
-  console.log(star);
-}
